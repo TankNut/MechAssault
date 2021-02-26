82f496e964767a8d7367ec462db7e0b467303a65
TankNut <m.vlasblom@live.nl>
2021-02-26 03:53:14 +0100

    Simplify crossbow math

--- lua/entities/ma2_mech/weapons/weapon_crossbow.lua
+++ lua/entities/ma2_mech/weapons/weapon_crossbow.lua
@@ -6,7 +6,6 @@ ENT.WeaponTypes.Crossbow = {
 	Function = "FireCrossbow",
 	Cooldown = {0.8, 1, 1.2},
 	FireRate = 0.1,
-	Repeats = {1, 2, 3},
 	Class = {
 		"ma2_proj_crossbow_lvl1",
 		"ma2_proj_crossbow_lvl2",
@@ -17,7 +16,7 @@ ENT.WeaponTypes.Crossbow = {
 
 function ENT:FireCrossbow(tbl, level, attachments)
 	if SERVER then
-		for i = 0, tbl.Repeats[level] - 1 do
+		for i = 0, level - 1 do
 			for k, v in ipairs(attachments) do
 				local count = k + (i * #attachments)
 
